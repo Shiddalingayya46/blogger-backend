d7cfb7ae2ef512ece9148e7bcdc610e2cb3e7ef1
manju <manjunathadur2001@gmail.com>
2025-08-08 23:54:36 +0530

    feat: add user logics

---
+++ config/db.js
@@ -0,0 +1,21 @@
+// config/db.js
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+async function connectToMongoDB() {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log('✅ Connected to MongoDB Atlas!');
+  } catch (err) {
+    console.error('❌ MongoDB connection error:');
+    console.error(err);
+    process.exit(1);
+  }
+}
+
+// ✅ Export the function
+module.exports = connectToMongoDB;
